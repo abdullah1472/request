3bc90edc999726286263146001802020974300cd
abdullah1472 <164834152+abdullah1472@users.noreply.github.com>
2024-05-11 06:02:17 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'طلبات',
+      title: 'نظام إدارة الطلبات',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(employeeName: 'عبدالله الصمعاني'),
     );
   }
 }
